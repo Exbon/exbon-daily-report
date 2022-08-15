d53905ba072e3a0ef9b621aa396fcba5969bebfd
HyunM <wisek0226@gmail.com>
2022-08-15 14:51:17 -0700

    dropdown

---
+++ pages/api/record/daily-report/equipment.js
@@ -0,0 +1,41 @@
+const mssql = require("mssql");
+const dbserver = require("../../../../dbConfig.js");
+
+const getEquipment = (req, res) => {
+  const { method, query } = req;
+  return new Promise(resolve => {
+    switch (method) {
+      case "GET":
+        mssql.connect(dbserver.dbConfig, err => {
+          if (err) {
+            console.error(err);
+            return resolve();
+          }
+          const request = new mssql.Request();
+
+          const sqlquery = `EXEC [Exbon].[dbo].[usp_dailyreport_Select_DailyReportEquipment_Dropdown]`;
+
+          request.query(sqlquery, (err, recordset) => {
+            if (err) {
+              console.error(err);
+              return resolve();
+            }
+            res.status(200).json({
+              message: "Success",
+              result: recordset.recordsets,
+            });
+            return resolve();
+          });
+        });
+        break;
+
+      default:
+        res.setHeader("Allow", ["GET"]);
+        res.status(405).end(`Method ${method} Not Allowed`);
+        res.status(404).end(`Failed`);
+        resolve();
+    }
+  });
+};
+
+export default getEquipment;
