27d29b04859f390dcfe796144966c06aa3135eb0
HyunM <wisek0226@gmail.com>
2023-04-21 12:23:54 -0700

    edit quip deleted

--- pages/api/record/daily-report/equipment.js
+++ pages/api/record/daily-report/equipment.js
@@ -71,7 +71,7 @@ const getEquipment = (req, res) => {
 					const request = new mssql.Request();
 
 					const sqlquery = `EXEC [dbo].[usp_dailyreport_Delete_DailyReportEquipment]
-								@projectID = ${body.ProjectID},
+								@recordID = ${body.RecordID},
 								`;
 					console.log(sqlquery);
 
