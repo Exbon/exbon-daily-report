b5351d62fe6e5b38890ed7a369fdb44c409fcaad
HyunM <wisek0226@gmail.com>
2023-04-21 11:04:19 -0700

    Deficiency Added

---
+++ pages/api/record/deficiency-log.js
@@ -0,0 +1,48 @@
+const mssql = require('mssql');
+const dbserver = require('../../../dbConfig.js');
+import { sqlEscape } from '../../../lib/utils';
+
+const addDeficiencyLog = (req, res) => {
+	const { method, query, body } = req;
+	return new Promise((resolve) => {
+		switch (method) {
+			case 'POST':
+				mssql.connect(dbserver.dbConfig, (err) => {
+					if (err) {
+						console.error(err);
+						return resolve();
+					}
+					const request = new mssql.Request();
+					const sqlquery = `EXEC [usp_dailyreport_Insert_Project_DeficiencyLog]
+                            @projectID = '${req.body.ProjectID}',
+                            @name = '${sqlEscape(req.body.DeficiencyName)}',
+                            @openedBy = '${req.body.OpenedBy}',
+                            @type = '${req.body.Type}',
+                            @trade = '${req.body.Trade || ''}',
+                            @problem = '${sqlEscape(req.body.Problem) || ''}',
+                            @userEmployeeID = ${EmployeeID}
+                        `;
+					request.query(sqlquery, (err, recordset) => {
+						if (err) {
+							console.error(err);
+							return resolve();
+						}
+						res.status(200).json({
+							message: 'Success',
+							result: recordset.recordsets,
+						});
+						return resolve();
+					});
+				});
+				break;
+
+			default:
+				res.setHeader('Allow', ['POST']);
+				res.status(405).end(`Method ${method} Not Allowed`);
+				res.status(404).end(`Failed`);
+				resolve();
+		}
+	});
+};
+
+export default addDeficiencyLog;
