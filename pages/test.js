acb327dd7cee2c7026b3307f12d3838c6f1373cd
HyunM <wisek0226@gmail.com>
2023-04-28 14:58:33 -0700

    only day

--- pages/test.js
+++ pages/test.js
@@ -27,15 +27,14 @@ export const test = () => {
 			elementsOfGridBody[0].innerHTML += test;
 
 			/* 2. Change column header text. ex) Sat, 15 -> 15 */
-			let elementsOfCalendar = document.getElementsByClassName('calendar');
-			elementsOfCalendar[0].innerHTML = elementsOfCalendar[0].innerHTML
-				.replace(/Mon, /g, '')
-				.replace(/Tue, /g, '')
-				.replace(/Wed, /g, '')
-				.replace(/Thu, /g, '')
-				.replace(/Fri, /g, '')
-				.replace(/Sat, /g, '')
-				.replace(/Sun, /g, '');
+			setTimeout(() => {
+				let elementsOfCalendar = document.getElementsByClassName('_9w8d5');
+				for (let i = 0; i < elementsOfCalendar.length; i++) {
+					elementsOfCalendar[i].innerHTML = elementsOfCalendar[i].innerHTML
+						.substring(4)
+						.trim();
+				}
+			}, 1);
 		} else {
 			/* remove highlights for weekends */
 			document.querySelectorAll('.weekend').forEach((el) => el.remove());
